5bc51ac78f7507d6eb71c80792a0e86cf68c1881
adambalcerzak <abalcerzak@soldevelo.com>
2026-05-18 12:06:14 +0200

    OBLS-513 Transfer Shortage Qty to Under Receipt Location on UI TODO for the future

--- grails-app/services/org/pih/warehouse/stockTransfer/StockTransferService.groovy
+++ grails-app/services/org/pih/warehouse/stockTransfer/StockTransferService.groovy
@@ -353,6 +353,9 @@ class StockTransferService {
 
         createStockTransferTransaction(stockTransfer, order)
 
+        // if (location is under receipt)
+        //      sendStockTransferNotification() -> fetch users with Stocktransfer notification role -> send
+
         return order
     }
 
